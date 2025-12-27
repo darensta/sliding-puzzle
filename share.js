ddb503be7d6d3802ec50447b1010bdc228667330
darensta <ren@cybren.com>
2025-12-26 19:20:04 -0800

    Update share.js

--- share.js
+++ share.js
@@ -2,15 +2,12 @@
    Canonical social sharing layer
 */
 
-export function shareOnFacebook(moves) {
-  const url = encodeURIComponent(window.location.href);
-
-  const quote = encodeURIComponent(
-    `I just solved the Edmunds.com sliding puzzle in ${moves} moves!`
-  );
-
-  const fbUrl =
-    `https://www.facebook.com/sharer/sharer.php?u=${url}&quote=${quote}`;
-
-  window.open(fbUrl, "_blank", "noopener,noreferrer,width=600,height=500");
+export function shareOnFacebook(moveCount) {
+  const url = window.location.href;
+  const quote = `I just solved the Edmunds.com tile puzzle in ${moveCount} moves`;
+  const shareUrl =
+    "https://www.facebook.com/sharer/sharer.php" +
+    `?u=${encodeURIComponent(url)}` +
+    `&quote=${encodeURIComponent(quote)}`;
+  window.open(shareUrl, "_blank");
 }
