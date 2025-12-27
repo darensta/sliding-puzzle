7859f6cdc0e94a1f042d79d54e018ba34c4a52f1
darensta <ren@cybren.com>
2025-12-26 18:45:28 -0800

    Update version.js

--- netlify/functions/version.js
+++ netlify/functions/version.js
@@ -2,9 +2,9 @@ exports.handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      commit: process.env.COMMIT_REF,
-      context: process.env.CONTEXT,
-      time: new Date().toISOString()
+      commit: process.env.COMMIT_REF || "unknown",
+      context: process.env.CONTEXT || "unknown",
+      deployTime: process.env.DEPLOY_TIME || process.env.DEPLOY_TIMESTAMP || "unknown"
     })
   };
 };
